102035fe4af016a1d6f203f648f5620756ea52a6
alexserga <snike1985@gmail.com>
2017-09-07 17:17:28 +0300

    fixed home page and left menu

--- app/assets/js/requirejs-config.js
+++ app/assets/js/requirejs-config.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         jquery: "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min",
+        jqueryui: "http://code.jquery.com/ui/1.10.3/jquery-ui",
         px: "pixeladmin",
         "px-libs": "libs",
         "px-bootstrap": "bootstrap",
